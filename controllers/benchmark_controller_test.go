5f58ef52a6a0d2df1dcf38cc433530e3feec0eef
Simon Zengerling <simon.zengerling@camunda.com>
2022-02-13 18:16:41 +0100

    feat: adds some integration tests

---
+++ controllers/benchmark_controller_test.go
@@ -0,0 +1,90 @@
+package controllers
+
+import (
+	"context"
+	v1 "k8s.io/api/apps/v1"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	benchmarkv1alpha1 "github.com/sijoma/camunda-benchmark-operator/api/v1alpha1"
+)
+
+// +kubebuilder:docs-gen:collapse=Imports
+
+var _ = Describe("CronJob controller", func() {
+
+	// Define utility constants for object names and testing timeouts/durations and intervals.
+	const (
+		BenchmarkName      = "test-benchmark"
+		BenchmarkNamespace = "default"
+
+		timeout  = time.Second * 10
+		duration = time.Second * 10
+		interval = time.Millisecond * 250
+	)
+
+	Context("When creating a Benchmark", func() {
+		It("Should start to be in progress running", func() {
+			By("By creating a new Benchmark")
+			ctx := context.Background()
+			benchmark := &benchmarkv1alpha1.Benchmark{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "cloud.camunda.io/v1alpha1",
+					Kind:       "Benchmark",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      BenchmarkName,
+					Namespace: BenchmarkNamespace,
+				},
+				Spec: benchmarkv1alpha1.BenchmarkSpec{
+					CredentialsSecretName: "test-credentials",
+					ProcessStarterRate:    100,
+					StarterReplicas:       50,
+					WorkerCount:           10,
+					Duration:              "2m",
+				},
+			}
+			Expect(k8sClient.Create(ctx, benchmark)).Should(Succeed())
+
+			benchmarkLookupKey := types.NamespacedName{Name: BenchmarkName, Namespace: BenchmarkNamespace}
+			createdBenchmark := &benchmarkv1alpha1.Benchmark{}
+
+			// We'll need to retry getting this newly created Benchmark, given that creation may not immediately happen.
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, benchmarkLookupKey, createdBenchmark)
+				if err != nil {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+
+			Expect(createdBenchmark.Spec.ProcessStarterRate).Should(Equal(100))
+
+			By("By checking the Benchmark is running")
+			Eventually(func() (string, error) {
+				err := k8sClient.Get(ctx, benchmarkLookupKey, createdBenchmark)
+				if err != nil {
+					return "", err
+				}
+				return createdBenchmark.Status.Progress, nil
+			}, duration, interval).Should(Equal("Running"))
+
+			By("By checking that the deployment count is two")
+			Eventually(func() (int, error) {
+				deploymentList := v1.DeploymentList{
+					TypeMeta: v1.Deployment{}.TypeMeta,
+				}
+				err := k8sClient.List(ctx, &deploymentList)
+				if err != nil {
+					return 0, err
+				}
+				return len(deploymentList.Items), err
+			}, duration, interval).Should(Equal(2))
+		})
+	})
+
+})
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,7 +17,9 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
 	"path/filepath"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -34,12 +36,11 @@ import (
 	//+kubebuilder:scaffold:imports
 )
 
-// These tests use Ginkgo (BDD-style Go testing framework). Refer to
-// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
-
 var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
+var ctx context.Context
+var cancel context.CancelFunc
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -52,6 +53,7 @@ func TestAPIs(t *testing.T) {
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
+	ctx, cancel = context.WithCancel(context.TODO())
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
@@ -71,9 +73,27 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&BenchmarkReconciler{
+		Client: k8sManager.GetClient(),
+		Scheme: k8sManager.GetScheme(),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+	}()
+
 }, 60)
 
 var _ = AfterSuite(func() {
+	cancel()
 	By("tearing down the test environment")
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
